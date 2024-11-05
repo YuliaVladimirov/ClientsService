4353eed61d4a008f75db47c97699f510e34f10f2
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-11-05 13:40:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Part of Microservice Based Online Shop
+
+#### A pet project
+
+
+## WiP
---
+++ src/test/java/org/example/clients_service/ClientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clients_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
